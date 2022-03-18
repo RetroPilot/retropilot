ac72a88b218dd5c399b1ceb3d5aaa8b4edb08318
jflorchi <jordan.florchinger@alumni.uleth.ca>
2022-03-18 12:14:05 -0700

    force corolla 2010

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+export FINGERPRINT="TOYOTA COROLLA 2010"
+export SKIP_FW_QUERY="True"
+
 export PASSIVE="0"
 export LD_LIBRARY_PATH="/system/lib64:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="/data/openpilot/third_party/snpe/aarch64:$LD_LIBRARY_PATH"
