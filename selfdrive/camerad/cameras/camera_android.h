4145314605bb980614b325704e93111602727f98
Cameron Clough <cameronjclough@gmail.com>
2022-04-30 14:56:52 +0100

    add utils

--- selfdrive/camerad/cameras/camera_android.h
+++ selfdrive/camerad/cameras/camera_android.h
@@ -34,6 +34,71 @@ static void CaptureSessionOnActive(void* context, ACameraCaptureSession* session
   LOG("Session is active.\n");
 }
 
+struct ImageFormat {
+  uint32_t width;
+  uint32_t height;
+};
+
+class DisplayDimension {
+public:
+  DisplayDimension(uint32_t w, uint32_t h) : w_(w), h_(h), portrait_(false) {
+    if (h > w) {
+      // make it landscape
+      w_ = h;
+      h_ = w;
+      portrait_ = true;
+    }
+  }
+
+  DisplayDimension(const DisplayDimension& other) : w_(other.w_), h_(other.h_), portrait_(other.portrait_) {}
+
+  DisplayDimension() : w_(0), h_(0), portrait_(false) {}
+
+  DisplayDimension& operator=(const DisplayDimension& other) {
+    w_ = other.w_;
+    h_ = other.h_;
+    portrait_ = other.portrait_;
+    return *this;
+  }
+
+  bool operator>(const DisplayDimension& other) const {
+    return (w_ > other.w_) || (h_ > other.h_);
+  }
+  bool operator==(const DisplayDimension& other) const {
+    return (w_ == other.w_) && (h_ == other.h_) && (portrait_ == other.portrait_);
+  }
+  DisplayDimension operator-(const DisplayDimension& other) const {
+    return DisplayDimension(w_ - other.w_, h_ - other.h_);
+  }
+
+  bool IsSameRatio(const DisplayDimension& other) const {
+    return (w_ * other.h_) == (h_ * other.w_);
+  }
+
+  void Flip() {
+    portrait_ = !portrait_;
+  }
+  bool IsPortrait() const {
+    return portrait_;
+  }
+  uint32_t width() const {
+    return w_;
+  }
+  uint32_t height() const {
+    return h_;
+  }
+  uint32_t org_width() const {
+    return portrait_ ? h_ : w_;
+  }
+  uint32_t org_height() const {
+    return portrait_ ? w_ : h_;
+  }
+
+private:
+    int32_t w_, h_;
+    bool portrait_;
+};
+
 typedef struct CameraState {
   CameraInfo ci;
   int camera_num;
