5430674b7853b3d5613c8709749819ed978311ed
Cameron Clough <cameronjclough@gmail.com>
2022-05-01 22:46:13 +0100

    delete callback

--- selfdrive/camerad/cameras/android/image_reader.cc
+++ selfdrive/camerad/cameras/android/image_reader.cc
@@ -7,11 +7,11 @@ ImageReader::ImageReader(ImageFormat *res, enum AIMAGE_FORMATS format)
                                           2, &reader_);
   assert(reader_ && status == AMEDIA_OK); // failed to create AImageReader
 
-  AImageReader_ImageListener listener{
-    .context = this,
-    .onImageAvailable = OnImageCallback,
-  };
-  AImageReader_setImageListener(reader_, &listener);
+  // AImageReader_ImageListener listener{
+  //   .context = this,
+  //   .onImageAvailable = OnImageCallback,
+  // };
+  // AImageReader_setImageListener(reader_, &listener);
 
   // assuming 4 bit per pixel max
   LOGE("Image Buffer Size: %d", res->width * res->height * 4);
@@ -37,32 +37,6 @@ ANativeWindow *ImageReader::GetNativeWindow() {
   assert(status == AMEDIA_OK); // failed to get native window
 
   return native_window;
-}
-
-void ImageReader::ImageCallback(AImageReader *reader) {
-  int32_t format;
-  media_status_t status = AImageReader_getFormat(reader, &format);
-  assert(status == AMEDIA_OK); // failed to get format
-
-  if (format == AIMAGE_FORMAT_JPEG) {
-    // Create a thread and write out the jpeg files
-    AImage *image = NULL;
-    status = AImageReader_acquireNextImage(reader, &image);
-    assert(status == AMEDIA_OK); // image is not available
-
-    int plane_count;
-    status = AImage_getNumberOfPlanes(image, &plane_count);
-    assert(status == AMEDIA_OK && plane_count == 1); // unsupported plane count
-
-    uint8_t *data = NULL;
-    int len;
-    status = AImage_getPlaneData(image, 0, &data, &len);
-    assert(status == AMEDIA_OK); // failed to get plane data
-
-    // TODO: do something with data and len??
-
-    AImage_delete(image);
-  }
 }
 
 AImage *ImageReader::GetLatestImage() {
--- selfdrive/camerad/cameras/android/image_reader.h
+++ selfdrive/camerad/cameras/android/image_reader.h
@@ -20,24 +20,10 @@ public:
 
   void DeleteImage(AImage *image);
 
-  void ImageCallback(AImageReader *reader);
+  void OnImageCallback(void *ctx, AImageReader *reader);
 
 private:
   AImageReader *reader_;
 
   uint8_t *image_buffer_;
 };
-
-/**
- * ImageReader listener: called by AImageReader for every frame captured
- * We pass the event to ImageReader class, so it could do some housekeeping
- * about
- * the loaded queue. For example, we could keep a counter to track how many
- * buffers are full and idle in the queue. If camera almost has no buffer to
- * capture
- * we could release ( skip ) some frames by AImageReader_getNextImage() and
- * AImageReader_delete().
- */
-void OnImageCallback(void *ctx, AImageReader *reader) {
-    reinterpret_cast<ImageReader *>(ctx)->ImageCallback(reader);
-}
