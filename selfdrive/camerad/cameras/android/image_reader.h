d3b3724ea09f4bbb94d3452287bce407523fbd5b
Cameron Clough <cameronjclough@gmail.com>
2022-05-01 20:34:09 +0100

    move around

--- selfdrive/camerad/cameras/android/image_reader.h
+++ selfdrive/camerad/cameras/android/image_reader.h
@@ -8,20 +8,6 @@
 #include "selfdrive/common/swaglog.h"
 #include "selfdrive/camerad/cameras/android/util.h"
 
-/**
- * ImageReader listener: called by AImageReader for every frame captured
- * We pass the event to ImageReader class, so it could do some housekeeping
- * about
- * the loaded queue. For example, we could keep a counter to track how many
- * buffers are full and idle in the queue. If camera almost has no buffer to
- * capture
- * we could release ( skip ) some frames by AImageReader_getNextImage() and
- * AImageReader_delete().
- */
-void OnImageCallback(void *ctx, AImageReader *reader) {
-    reinterpret_cast<ImageReader *>(ctx)->ImageCallback(reader);
-}
-
 class ImageReader {
 public:
   explicit ImageReader(ImageFormat *res, enum AIMAGE_FORMATS format);
@@ -44,3 +30,17 @@ private:
 
   uint8_t *image_buffer_;
 };
+
+/**
+ * ImageReader listener: called by AImageReader for every frame captured
+ * We pass the event to ImageReader class, so it could do some housekeeping
+ * about
+ * the loaded queue. For example, we could keep a counter to track how many
+ * buffers are full and idle in the queue. If camera almost has no buffer to
+ * capture
+ * we could release ( skip ) some frames by AImageReader_getNextImage() and
+ * AImageReader_delete().
+ */
+void OnImageCallback(void *ctx, AImageReader *reader) {
+    reinterpret_cast<ImageReader *>(ctx)->ImageCallback(reader);
+}
