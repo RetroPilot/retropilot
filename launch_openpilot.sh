1d66782c56f324aef23ef87d2141e056e92e2971
Cameron Clough <cameronjclough@gmail.com>
2022-05-02 20:53:34 +0100

    path

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -2,6 +2,7 @@
 
 export PASSIVE="0"
 export LD_LIBRARY_PATH="/system/lib64:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/data/openpilot/third_party/snpe/aarch64:$LD_LIBRARY_PATH"
 export LOGPRINT="debug"
 exec ./launch_chffrplus.sh
 
