cacf3c733ed920d75e314b57ed96a9318f674b54
Cameron Clough <cameronjclough@gmail.com>
2022-05-03 00:41:11 +0100

    refactor2

---
+++ selfdrive/camerad/cameras/android/image_format.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/*
+ * ImageFormat:
+ *     A Data Structure to communicate resolution between camera and ImageReader
+ */
+struct ImageFormat {
+  int32_t width;
+  int32_t height;
+
+  int32_t format; // Through out this demo, the format is fixed to
+                  // YUV_420 format
+}
