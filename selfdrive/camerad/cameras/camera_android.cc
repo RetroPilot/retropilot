a852ba1eb545f56f0fe6baead357e61f05fc1675
Cameron Clough <cameronjclough@gmail.com>
2022-05-03 00:55:47 +0100

    print formats

--- selfdrive/camerad/cameras/camera_android.cc
+++ selfdrive/camerad/cameras/camera_android.cc
@@ -305,6 +305,23 @@ void cameras_init(VisionIpcServer *v, MultiCameraState *s, cl_device_id device_i
                                                      &metadata);
     assert(status == ACAMERA_OK);  // failed to get camera characteristics
 
+    ACameraMetadata_const_entry entry;
+    status = ACameraMetadata_getConstEntry(metadata,
+                                           ACAMERA_SCALER_AVAILABLE_STREAM_CONFIGURATIONS,
+                                           &entry);
+
+    // format of the data: format, width, height, input?, type int 32
+    for (int i = 0; i < entry.count; i += 4) {
+      int32_t input = entry.data.i32[i + 3];
+      int32_t format = entry.data.i32[i + 0];
+      if (input) continue;
+
+      const char *format_name = format == AIMAGE_FORMAT_YUV_420_888 ? "YUV_420_888" : (format == AIMAGE_FORMAT_JPEG ? "JPEG" : "unknown");
+      int32_t width = entry.data.i32[i + 1];
+      int32_t height = entry.data.i32[i + 2];
+      LOG("Camera %s supports format %s: %dx%d", id, format_name, width, height);
+    }
+
     int32_t count = 0;
     const uint32_t* tags = nullptr;
     status = ACameraMetadata_getAllTags(metadata, &count, &tags);
