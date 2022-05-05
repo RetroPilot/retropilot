957b45165c43b4df6213c251a6381b9bdfd57afe
Cameron Clough <cameronjclough@gmail.com>
2022-05-05 01:25:48 +0100

    oop

--- selfdrive/camerad/cameras/camera_android.cc
+++ selfdrive/camerad/cameras/camera_android.cc
@@ -1,5 +1,7 @@
 #include "selfdrive/camerad/cameras/camera_android.h"
 
+#include <camera/NdkCameraError.h>
+
 #include "selfdrive/common/swaglog.h"
 
 // id of the video capturing device
@@ -41,206 +43,62 @@ void CameraState::camera_init(MultiCameraState *multi_cam_state_, VisionIpcServe
 
   camera_num = camera_index;
   fps = fps_;
-  // TODO: fix me
   buf.init(device_id, ctx, this, v, FRAME_BUF_COUNT, rgb_type, yuv_type);
 
   // ASSUMPTION: IXM363 (road) is index[0] and IMX355 (driver) is index[1]
   // TODO: check that we actually need to rotate
-  if (camera_id_ == CAMERA_ID_IMX363) {
-    camera_orientation = 90;
-  } else if (camera_id_ == CAMERA_ID_IMX355) {
-    camera_orientation = 270;
-  }
+  // if (camera_id_ == CAMERA_ID_IMX363) {
+  //   camera_orientation = 90;
+  // } else if (camera_id_ == CAMERA_ID_IMX355) {
+  //   camera_orientation = 270;
+  // }
 
-  // ** get android camera id **
-  ACameraManager *camera_manager = multi_cam_state->camera_manager;
-  ACameraIdList *camera_id_list = NULL;
+  enum AIMAGE_FORMATS format = AIMAGE_FORMAT_YUV_420_888;
 
-  LOGD("camera_init: getting camera list");
-  camera_status_t camera_status = ACameraManager_getCameraIdList(camera_manager, &camera_id_list);
-  assert(camera_status == ACAMERA_OK);
+  native_camera = new NativeCamera(camera_index);
+  native_camera->match_capture_size_request(&view, ci.frame_width, ci.frame_height, format);
+  assert(view.width && view.height);
 
-  LOGD("camera_init: found %d cameras", camera_id_list->numCameras);
-  assert(camera_id_list->numCameras > 0);
-
-  // note: we assume that the first camera is the road facing camera and the
-  // second is the driver facing camera
-  camera_id = camera_id_list->cameraIds[camera_index];
-
-  // TODO: after we figure out how to copy the camera_id
-  // ACameraManager_deleteCameraIdList(camera_id_list);
-}
-
-void create_image_reader(CameraInfo *ci, AIMAGE_FORMATS format, AImageReader **reader, ANativeWindow **window) {
-  media_status_t status;
-
-  // new image reader
-  status = AImageReader_new(ci->frame_width, ci->frame_height, format, 2, reader);
-  assert(*reader && status == AMEDIA_OK);
-
-  // get native window
-  status = AImageReader_getWindow(*reader, window);
-  assert(*window && status == AMEDIA_OK);
-}
-
-void open_camera(ACameraManager *manager, const char *camera_id, ACameraDevice_StateCallbacks *listener, ACameraDevice **device) {
-  camera_status_t status;
-
-  // open camera device
-  status = ACameraManager_openCamera(manager, camera_id, listener, device);
-  assert(*device && status == ACAMERA_OK);
-}
-
-void CameraState::match_camera_size(int32_t width, int32_t height, enum AIMAGE_FORMATS desired_format) {
-  LOGD("match_camera_size: w=%d, h=%d, format=0x%X", width, height, desired_format);
-
-  DisplayDimension disp(width, height);
-
-  ACameraMetadata *metadata = nullptr;
-  ACameraManager_getCameraCharacteristics(multi_cam_state->camera_manager, camera_id, &metadata);
-  ACameraMetadata_const_entry entry;
-  ACameraMetadata_getConstEntry(metadata, ACAMERA_SCALER_AVAILABLE_STREAM_CONFIGURATIONS, &entry);
-  // format of the data: format, width, height, input?, type int32
-
-  bool foundIt = false;
-  DisplayDimension foundRes(1920, 1080);
-
-  for (int i = 0; i < entry.count; i++) {
-    int32_t input = entry.data.i32[i * 4 + 3];
-    int32_t format = entry.data.i32[i * 4 + 0];
-
-    if (input) continue;
-
-    DisplayDimension res(entry.data.i32[i * 4 + 1],
-                         entry.data.i32[i * 4 + 2]);
-
-    if (format) {
-      LOGD("found format 0x%X, w: %d, h: %d", format, res.width(), res.height());
-    }
-
-    if (format == desired_format && width == res.width() && height == res.height()) {
-      foundIt = true;
-      foundRes = res;
-      break;
-    }
-  }
-
-  if (foundIt) {
-    view.width = foundRes.org_width();
-    view.height = foundRes.org_height();
-    LOGD("found width=%d, height=%d", view.width, view.height);
-  } else {
-    LOGW("could not find a matching resolution");
-  }
-}
-
-void CameraState::create_session(ANativeWindow *window, ACameraDevice *device) {
-  camera_status_t status;
-
-  // create output container
-  status = ACaptureSessionOutputContainer_create(&capture_session_output_container);
-  assert(capture_session_output_container && status == ACAMERA_OK);
-
-  // create output to native window
-  ANativeWindow_acquire(window);
-  status = ACaptureSessionOutput_create(window, &capture_session_output);
-  assert(capture_session_output && status == ACAMERA_OK);
-  status = ACaptureSessionOutputContainer_add(capture_session_output_container, capture_session_output);
-  assert(status == ACAMERA_OK);
-  status = ACameraOutputTarget_create(window, &camera_output_target);
-  assert(camera_output_target && status == ACAMERA_OK);
-  status = ACameraDevice_createCaptureRequest(device, TEMPLATE_RECORD, &capture_request);
-  assert(capture_request && status == ACAMERA_OK);
-  status = ACaptureRequest_addTarget(capture_request, camera_output_target);
-  assert(status == ACAMERA_OK);
-
-  // create capture session
-  status = ACameraDevice_createCaptureSession(device, capture_session_output_container,
-                                              get_session_listener(), &capture_session);
-  assert(capture_session && status == ACAMERA_OK);
-  LOGD("create_session: created capture session");
-
-  // TODO: manual mode
-}
-
-void CameraState::start_preview(bool start) {
-  camera_status_t status;
-
-  if (start) {
-    status = ACameraCaptureSession_setRepeatingRequest(capture_session, nullptr, 1, &capture_request, nullptr);
-    if (status) LOGE("ACameraCaptureSession_setRepeatingRequest failed: %d", status);
-    assert(status == ACAMERA_OK);
-  } else {
-    status = ACameraCaptureSession_stopRepeating(capture_session);
-    assert(status == ACAMERA_OK);
-  }
-}
-
-void CameraState::camera_open() {
-  LOGD("camera_open camera_num=%d camera_id=\"%s\"", camera_num, camera_id);
-
-  ACameraManager *manager = multi_cam_state->camera_manager;
-
-  match_camera_size(ci.frame_width, ci.frame_height, AIMAGE_FORMAT_YUV_420_888);
-  LOGD("camera_open: view w=%d, h=%d", view.width, view.height);
-
-  create_image_reader(&ci, AIMAGE_FORMAT_YUV_420_888, &yuv_reader, &yuv_window);
-  LOGD("camera_open: yuv_reader=%p, yuv_window=%p", yuv_reader, yuv_window);
-
-  open_camera(manager, camera_id, get_device_listener(), &camera_device);
-  LOGD("camera_open: camera_device=%p", camera_device);
-
-  create_session(yuv_window, camera_device);
-  LOGD("camera_open: capture_session=%p", capture_session);
+  image_reader = new ImageReader(&view, format);
 }
 
 void CameraState::camera_run(float *ts) {
   LOGD("camera_run %d", camera_num);
 
-  // TODO: implement transform
-  // cv::Size size(ci.frame_width, ci.frame_height);
-  // const cv::Mat transform = cv::Mat(3, 3, CV_32F, ts);
-
   uint32_t frame_id = 0;
   size_t buf_idx = 0;
 
-  start_preview(true);
+  native_camera->start_preview(true);
 
   while (!do_exit) {
-    // ** get image **
-    AImage *image = NULL;
-    media_status_t status = AImageReader_acquireLatestImage(yuv_reader, &image);
-    if (status != AMEDIA_OK) {
-      if (status != AMEDIA_IMGREADER_NO_BUFFER_AVAILABLE) {
-        LOGW("camera_run: AImageReader_acquireLatestImage status %d", status);
-      }
+    AImage *image = image_reader->GetLatestImage();
+    if (!image) {
       util::sleep_for(1);
       continue;
     }
 
     LOGD("camera_run: image=%p", image);
 
-    // ** debug **
     int32_t format = -1;
-    AImage_getFormat(image, &format);
-    assert(format == AIMAGE_FORMAT_YUV_420_888);
+    media_status_t status = AImage_getFormat(image, &format);
+    assert(status == AMEDIA_OK && format == AIMAGE_FORMAT_YUV_420_888);
 
     int32_t planeCount = 0;
-    AImage_getNumberOfPlanes(image, &planeCount);
-    assert(planeCount == 3);
+    status = AImage_getNumberOfPlanes(image, &planeCount);
+    assert(status == AMEDIA_OK && planeCount == 3);
 
-    buf.camera_bufs_metadata[buf_idx] = {.frame_id = frame_id};
+    uint8_t *y_data, *u_data, *v_data;
+    int y_len, u_len, v_len;
 
-    // ** copy image data to cl buffer **
-    uint8_t *data = NULL;
-    int size = 0;
-    status = AImage_getPlaneData(image, 0, &data, &size);
-    assert(status == AMEDIA_OK);  // failed to get image data
+    status = AImage_getPlaneData(image, 0, &y_data, &y_len);
+    assert(status == AMEDIA_OK);
+    status = AImage_getPlaneData(image, 1, &u_data, &u_len);
+    assert(status == AMEDIA_OK);
+    status = AImage_getPlaneData(image, 2, &v_data, &v_len);
+    assert(status == AMEDIA_OK);
 
-    auto &buffer = buf.camera_bufs[buf_idx];
-    LOGD("camera_run: clEnqueueWriteBuffer size=%d", size);
-    CL_CHECK(clEnqueueWriteBuffer(buffer.copy_q, buffer.buf_cl, CL_TRUE, 0, size, data, 0, NULL, NULL));
-    LOGD("camera_run: clEnqueueWriteBuffer done");
+    LOGD("camera_run: y_data=%p, u_data=%p, v_data=%p", y_data, u_data, v_data);
+    LOGD("camera_run: y_len=%d, u_len=%d, v_len=%d", y_len, u_len, v_len);
 
     // ** release image **
     AImage_delete(image);
@@ -248,114 +106,22 @@ void CameraState::camera_run(float *ts) {
     buf.queue(buf_idx);
 
     ++frame_id;
-    buf_idx = (buf_idx + 1) % FRAME_BUF_COUNT;
   }
 
-  start_preview(false);
+  native_camera->start_preview(false);
 }
 
 void CameraState::camera_close() {
   LOGD("camera_close %d", camera_num);
 
-  if (capture_session) {
-    ACameraCaptureSession_close(capture_session);
-    capture_session = NULL;
-  }
-
-  ACaptureRequest_removeTarget(capture_request, camera_output_target);
-  ACameraOutputTarget_free(camera_output_target);
-  camera_output_target = NULL;
-
-  ACaptureRequest_free(capture_request);
-  capture_request = NULL;
-
-  if (camera_device) {
-    ACameraDevice_close(camera_device);
-    camera_device = NULL;
-  }
-
-  if (capture_session_output_container) {
-    if (capture_session_output) {
-      ACaptureSessionOutputContainer_remove(capture_session_output_container,
-                                            capture_session_output);
-
-      ACaptureSessionOutput_free(capture_session_output);
-      capture_session_output = NULL;
-    }
-
-    ACaptureSessionOutputContainer_free(capture_session_output_container);
-    capture_session_output_container = NULL;
-  }
-
-  if (yuv_reader) {
-    AImageReader_delete(yuv_reader);
-    yuv_reader = NULL;
-  }
-}
-
-// ** CameraDevice callbacks **
-
-static void OnDeviceDisconnect(void* /* ctx */, ACameraDevice* dev) {
-  std::string id(ACameraDevice_getId(dev));
-  LOGW("Device \"%s\" disconnected", id.c_str());
-}
-
-static void OnDeviceError(void* /* ctx */, ACameraDevice* dev, int err) {
-  std::string id(ACameraDevice_getId(dev));
-  LOGE("Camera Device Error: %#x, Device \"%s\"", err, id.c_str());
-
-  switch (err) {
-    case ERROR_CAMERA_IN_USE:
-      LOGE("Camera in use");
-      break;
-    case ERROR_CAMERA_SERVICE:
-      LOGE("Fatal Error occured in Camera Service");
-      break;
-    case ERROR_CAMERA_DEVICE:
-      LOGE("Fatal Error occured in Camera Device");
-      break;
-    case ERROR_CAMERA_DISABLED:
-      LOGE("Camera disabled");
-      break;
-    case ERROR_MAX_CAMERAS_IN_USE:
-      LOGE("System limit for maximum concurrent cameras used was exceeded");
-      break;
-    default:
-      LOGE("Unknown Camera Device Error: %#x", err);
-  }
-}
-
-ACameraDevice_StateCallbacks *CameraState::get_device_listener() {
-  static ACameraDevice_stateCallbacks device_listener = {
-    .context = this,
-    .onDisconnected = ::OnDeviceDisconnect,
-    .onError = ::OnDeviceError,
-  };
-  return &device_listener;
-}
-
-// ** CaptureSession callbacks **
-
-void OnSessionClosed(void *context, ACameraCaptureSession *session) {
-  LOGW("session %p closed", session);
-}
-
-void OnSessionReady(void *context, ACameraCaptureSession *session) {
-  LOGD("session %p ready", session);
-}
-
-void OnSessionActive(void *context, ACameraCaptureSession *session) {
-  LOGD("session %p active", session);
-}
-
-ACameraCaptureSession_stateCallbacks *CameraState::get_session_listener() {
-  static ACameraCaptureSession_stateCallbacks session_listener = {
-    .context = this,
-    .onActive = ::OnSessionActive,
-    .onReady = ::OnSessionReady,
-    .onClosed = ::OnSessionClosed,
-  };
-  return &session_listener;
+  // if (image_reader) {
+  //   delete image_reader;
+  //   image_reader = nullptr;
+  // }
+  // if (native_camera) {
+  //   delete native_camera;
+  //   native_camera = nullptr;
+  // }
 }
 
 #if ROAD
--- selfdrive/camerad/cameras/camera_android.h
+++ selfdrive/camerad/cameras/camera_android.h
@@ -3,10 +3,6 @@
 #include <android/native_window.h>
 #include <camera/NdkCameraCaptureSession.h>
 #include <camera/NdkCameraDevice.h>
-#include <camera/NdkCameraError.h>
-#include <camera/NdkCameraManager.h>
-#include <camera/NdkCameraMetadata.h>
-#include <camera/NdkCameraMetadataTags.h>
 #include <camera/NdkCaptureRequest.h>
 #include <media/NdkImageReader.h>
 
@@ -19,74 +15,10 @@
 #include "selfdrive/common/timing.h"
 #include "selfdrive/common/util.h"
 #include "selfdrive/camerad/cameras/camera_common.h"
+#include "selfdrive/camerad/cameras/android/image_reader.h"
+#include "selfdrive/camerad/cameras/android/native_camera.h"
 
 #define FRAME_BUF_COUNT 4
-
-struct ImageFormat {
-  int32_t width;
-  int32_t height;
-};
-
-/**
- * A helper class to assist image size comparison, by comparing the absolute
- * size
- * regardless of the portrait or landscape mode.
- */
-class DisplayDimension {
- public:
-  DisplayDimension(int32_t w, int32_t h) : w_(w), h_(h), portrait_(false) {
-    if (h > w) {
-      // make it landscape
-      w_ = h;
-      h_ = w;
-      portrait_ = true;
-    }
-  }
-
-  DisplayDimension(const DisplayDimension& other) {
-    w_ = other.w_;
-    h_ = other.h_;
-    portrait_ = other.portrait_;
-  }
-
-  DisplayDimension() {
-    w_ = 0;
-    h_ = 0;
-    portrait_ = false;
-  }
-
-  DisplayDimension& operator=(const DisplayDimension& other) {
-    w_ = other.w_;
-    h_ = other.h_;
-    portrait_ = other.portrait_;
-
-    return (*this);
-  }
-
-  bool IsSameRatio(DisplayDimension& other) {
-    return (w_ * other.h_ == h_ * other.w_);
-  }
-  bool operator>(DisplayDimension& other) {
-    return (w_ >= other.w_) || (h_ >= other.h_);
-  }
-  bool operator==(DisplayDimension& other) {
-    return w_ == other.w_ && h_ == other.h_ && portrait_ == other.portrait_;
-  }
-  DisplayDimension operator-(DisplayDimension& other) {
-    return DisplayDimension(w_ - other.w_, h_ - other.h_);
-  }
-
-  void Flip() { portrait_ = !portrait_; }
-  bool IsPortrait() { return portrait_; }
-  int32_t width() { return w_; }
-  int32_t height() { return h_; }
-  int32_t org_width() { return (portrait_ ? h_ : w_); }
-  int32_t org_height() { return (portrait_ ? w_ : h_); }
-
-private:
-  int32_t w_, h_;
-  bool portrait_;
-};
 
 class CameraState {
 public:
@@ -99,32 +31,15 @@ public:
 private:
   MultiCameraState *multi_cam_state;
 
-  ACameraDevice *camera_device;
-  int32_t camera_orientation;
-  const char *camera_id;
-
   ImageFormat view{0, 0};
-  AImageReader *yuv_reader;
-  ANativeWindow *yuv_window;
-
-  ACaptureSessionOutputContainer *capture_session_output_container;
-  ACaptureRequest *capture_request;
-  ACaptureSessionOutput *capture_session_output;
-  ACameraOutputTarget *camera_output_target;
-  ACameraCaptureSession *capture_session;
+  NativeCamera *native_camera;
+  ImageReader *image_reader;
 
 public:
   void camera_init(MultiCameraState *multi_cam_state_, VisionIpcServer *v, int camera_index, int camera_id_, unsigned int fps, cl_device_id device_id, cl_context ctx, VisionStreamType rgb_type, VisionStreamType yuv_type);
   void camera_open();
   void camera_run(float *ts);
   void camera_close();
-
-  void match_camera_size(int32_t width, int32_t height, enum AIMAGE_FORMATS desired_format);
-  void create_session(ANativeWindow *window, ACameraDevice *device);
-  void start_preview(bool start);
-
-  ACameraDevice_StateCallbacks *get_device_listener();
-  ACameraCaptureSession_stateCallbacks *get_session_listener();
 };
 
 
