fa454482752210655c7de0d3e9a2110913f45360
Cameron Clough <cameronjclough@gmail.com>
2022-05-06 20:13:55 +0100

    run camera

---
+++ selfdrive/camerad/run_camera.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+cd "$(dirname "$0")"
+export LD_LIBRARY_PATH="/system/lib64:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/data/openpilot/third_party/snpe/aarch64:$LD_LIBRARY_PATH"
+export LOGPRINT="debug"
+exec ./camerad
